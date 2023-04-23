8be5d4f3b097a40570a8d8172ecdaa10a7b714f5
Jonas Kolb <jonas.kolb28@gmail.com>
2023-04-23 19:42:10 +0200

    added comments

--- auswertung_oszilloskop.py
+++ auswertung_oszilloskop.py
@@ -2,8 +2,10 @@ import csv
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Path of the file that is being analized
 filepath = r"C:\Users\jonas\Desktop\TPA_Kolb\Oszilloskop Messungen 19.4.23\WA000001.CSV"
 
+# Reads in the file
 with open(filepath, 'r') as file:
     reader = csv.reader(file)
     
@@ -14,11 +16,11 @@ with open(filepath, 'r') as file:
 
 file.close()
 
+# Splits the lines in Header and Data
 header = lines[:10]
 data = lines[10:]
 
-# print(header)
-# print(data[0])
+# Splits the data in seperate arrays
 time = []
 voltage_total = []
 voltage_shunt = []
@@ -26,12 +28,8 @@ for line in data:
     time.append(float(line[3]))
     voltage_total.append(float(line[4]))
     voltage_shunt.append(float(line[5]))
-    
-# voltage_dut = []
-# for i in range(len(voltage_total)):
-#     voltage_dut.append(voltage_total[i] - voltage_shunt[i])  
 
-    
+# Calculates the fft coefficients of each voltage signal    
 fft_coeff_voltage_total = 2 * np.fft.fft(voltage_total) / len(data)
 fft_coeff_voltage_shunt = 2 * np.fft.fft(voltage_shunt) / len(data)
 
@@ -42,17 +40,19 @@ fft_coeff_voltage_dut = []
 for i in range(len(fft_coeff_voltage_total)):
     fft_coeff_voltage_dut.append(fft_coeff_voltage_total[i] - fft_coeff_voltage_shunt[i])
 
+# Calculates the current and its fft coefficients from the shunt voltage signal
 current = list(map(lambda x: x / 100, voltage_shunt))
 
 fft_coeff_current = 2 * np.fft.fft(current) / len(data)
 
+# Calculates the magnitudes and phases of the voltage and current of the DUT
 magnitudes_voltage_dut = np.abs(fft_coeff_voltage_dut)
 magnitudes_current = np.abs(fft_coeff_current)
 
 phases_voltage_dut = np.angle(fft_coeff_voltage_dut)
 phases_current = np.angle(fft_coeff_current) 
 
-
+# Creates the plots
 fig, ax = plt.subplots(2, 2, figsize=(12, 7))
 ax[0][0].plot(time, voltage_total, label='Total Voltage')
 ax[0][0].plot(time, voltage_shunt, label='Shunt Voltage')
@@ -62,10 +62,9 @@ ax[0][1].semilogx(time, magnitudes_voltage_dut, label="Magnitudes DUT")
 ax[0][1].semilogx(time, magnitudes_current, label="Magnitudes Current")
 ax[0][1].legend()
 
+
 magnitude_impedance = max(magnitudes_voltage_dut) / max(magnitudes_current)
 print(magnitude_impedance)
-# ax[1][0].semilogx(frequency_vector, magnitudes_impedance)
-# ax[1][0].semilogx(frequency_vector, magnitudes_current)
 
-# plt.show()
+plt.show()
 
