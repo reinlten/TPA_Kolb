53bb121b29de53148d8b9f67dda132c36ce494fd
Jonas Kolb <jonas.kolb28@gmail.com>
2023-04-23 23:25:12 +0200

    some changes and fixes with the dc part

--- auswertung_oszilloskop.py
+++ auswertung_oszilloskop.py
@@ -5,6 +5,8 @@ import numpy as np
 # Path of the file that is being analized
 filepath = r"C:\Users\jonas\Desktop\TPA_Kolb\Oszilloskop Messungen 19.4.23\WA000001.CSV"
 
+number_of_samples = 9000
+
 # Reads in the file
 with open(filepath, 'r') as file:
     reader = csv.reader(file)
@@ -15,10 +17,12 @@ with open(filepath, 'r') as file:
         lines.append(row)     
 
 file.close()
+print(len(lines))
 
 # Splits the lines in Header and Data
 header = lines[:10]
-data = lines[10:]
+data = lines[10:number_of_samples + 10]
+print(len(data))
 
 # Splits the data in seperate arrays
 time = []
@@ -33,8 +37,8 @@ for line in data:
 fft_coeff_voltage_total = 2 * np.fft.fft(voltage_total) / len(data)
 fft_coeff_voltage_shunt = 2 * np.fft.fft(voltage_shunt) / len(data)
 
-fft_coeff_voltage_total[0] / 2
-fft_coeff_voltage_shunt[0] / 2
+fft_coeff_voltage_total[0] /= 2
+fft_coeff_voltage_shunt[0] /= 2
 
 fft_coeff_voltage_dut = []
 for i in range(len(fft_coeff_voltage_total)):
@@ -45,6 +49,8 @@ current = list(map(lambda x: x / 100, voltage_shunt))
 
 fft_coeff_current = 2 * np.fft.fft(current) / len(data)
 
+fft_coeff_current[0] /= 2
+
 # Calculates the magnitudes and phases of the voltage and current of the DUT
 magnitudes_voltage_dut = np.abs(fft_coeff_voltage_dut)
 magnitudes_current = np.abs(fft_coeff_current)
@@ -52,14 +58,20 @@ magnitudes_current = np.abs(fft_coeff_current)
 phases_voltage_dut = np.angle(fft_coeff_voltage_dut)
 phases_current = np.angle(fft_coeff_current) 
 
+sample_frequency = 1000000000
+df = sample_frequency / number_of_samples
+print(df)
+frequency_vector = np.arange(0, number_of_samples * df, df)
+print(frequency_vector[:10])
+
 # Creates the plots
 fig, ax = plt.subplots(2, 2, figsize=(12, 7))
 ax[0][0].plot(time, voltage_total, label='Total Voltage')
 ax[0][0].plot(time, voltage_shunt, label='Shunt Voltage')
 ax[0][0].legend()
 
-ax[0][1].semilogx(time, magnitudes_voltage_dut, label="Magnitudes DUT")
-ax[0][1].semilogx(time, magnitudes_current, label="Magnitudes Current")
+ax[0][1].semilogx(frequency_vector, magnitudes_voltage_dut, label="Magnitudes DUT")
+ax[0][1].semilogx(frequency_vector, magnitudes_current, label="Magnitudes Current")
 ax[0][1].legend()
 
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,1 @@
-  # ax[0][1].semilogx(frequency_vector, magnitudes_voltage_dut)
-    # ax[0][1].semilogx(frequency_vector, magnitudes_current)
+
