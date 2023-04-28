93db90644e6b1a4cb22f1a1a0671fc988bf41ec5
Jonas Kolb <jonas.kolb28@gmail.com>
2023-04-28 13:14:53 +0200

    added comments

--- evaluation_bbb.py
+++ evaluation_bbb.py
@@ -4,20 +4,22 @@ import math
 import os
 import re
 
+# Pfad wo die Ordner mit den Messungen liegen
 measurement_path = r"C:\Users\jonas\Desktop\Teamprojektarbeit\Aktuell\Messungen BBB\07.02.2023_1"
 
-number_of_samples = 100000
-
+# Arrays für die Speicherung der Amplituden, Phasen und Frequenzen
 magnitudes = []
 phases = []
 frequencies = []
 
+# Hilfsfunktion zur Sortierung der Dateien
 def extract_number(file_path):
     match = re.search(r'\d+', file_path)
     if match:
         return int(match.group(0))
     return 0
 
+# Durchsuchen und Aneinanderreihung der Dateipfade
 folder_paths = os.listdir(measurement_path)
 folder_paths = sorted(folder_paths, key=extract_number)
 
@@ -26,65 +28,77 @@ for folder in folder_paths:
     file_paths = os.listdir(folder_path)
     file_paths = sorted(file_paths, key=extract_number)
 
+    # Ab hier Auswertung einer Textdatei
     for file in file_paths:
         file_path = os.path.join(folder_path, file)
         
-        number_of_samples = 100000
-
+        # Textdatei wird gelesen
         with open(file_path, "r") as file:
             lines = file.readlines()
 
         file.close()
 
+        # Header und Daten werden geteilt
         header = lines[0]
         data = lines[1:]
         
+        # Header wird ausgelesen und gespeichert
         current_freq, sample_freq, number_of_periods, number_of_samples_per_period = header.split()
         
+        # Headerwerte werden in die richtige Form gebracht
         current_freq = int(current_freq)
         sample_freq = int(sample_freq)
         number_of_periods = int(number_of_periods)
         number_of_samples_per_period = int(number_of_samples_per_period)
         
-        number_of_samples = 10 * (sample_freq // current_freq)
+        # Anzahl der Dateipunkte wird über Periodenanzahl bestimmt
+        periods_wanted = 10
+        number_of_samples = periods_wanted * (sample_freq // current_freq)
         
+        # Datenwerte werden in Gesamtspannung (voltage1) und Shuntspannung (voltage2) geteilt
         voltage1 = []
         voltage2 = []
-    
 
         for i in range(number_of_samples):
                 first, second = data[i].split()
                 voltage1.append(int(first))
                 voltage2.append(int(second))
        
+       # Kompensationsfaktor für 10 kOhm Widerstand wird berechnet und Spannung wird damit multipliziert
         factor = 4700 / (max(voltage1) / 2)
 
         for i in range(len(voltage1)):
             voltage1[i] *= factor
         
         print(f"Factor: {factor}")
-
+        
+        # Größen for die Visualisierung werden berechnet
         dt = 1 / sample_freq
         df = sample_freq / number_of_samples
         time_vector = np.arange(0, number_of_samples * dt, dt)
         frequency_vector = np.arange(0, number_of_samples * df, df)
         
+        # Fourierkoeffizienten der beiden Spannungen werden berechnet 
         fft_coefficients_voltage1 = 2 * np.fft.fft(voltage1) / number_of_samples
         fft_coefficients_voltage2 = 2 * np.fft.fft(voltage2) / number_of_samples
 
         fft_coefficients_voltage1[0] /= 2
         fft_coefficients_voltage2[0] /= 2
         
+        # Fourierkoeffizienten der DUT-Spannung wird berechnet
         fft_coefficients_voltage_dut = []
         for i in range(len(fft_coefficients_voltage1)):
             fft_coefficients_voltage_dut.append(fft_coefficients_voltage1[i] - fft_coefficients_voltage2[i])
 
+        # Stromstärkewerte durch DUT werden über Ohmsches Gesetz und Shuntspannung berechnet
         current = list(map(lambda x: x / 100, voltage2))
-        
+
+        # Fourrierkoeffizienten des Stroms werden berechnet
         fft_coefficients_current = 2 * np.fft.fft(current) / number_of_samples
         
         fft_coefficients_current[0] /= 2
         
+        # Amplituden der DUT-Spannuns und des DUT-Stroms werden berechnet
         magnitudes_voltage_dut = np.abs(fft_coefficients_voltage_dut)
         magnitudes_current = np.abs(fft_coefficients_current)
 
@@ -92,13 +106,16 @@ for folder in folder_paths:
         phases_voltage_dut = np.angle(fft_coefficients_voltage_dut)
         phases_current = np.angle(fft_coefficients_current) 
 
+        # Daten der aktuellen Frequenz werden ausgegeben
         print(f"Current frequency: {current_freq}")
 
         print(f"Magnitude of DC component of voltage: {magnitudes_voltage_dut[0]} mV")
         print(f"Magnitude of DC component of current: {magnitudes_current[0]} mA")
 
+        # Position der Werte der aktuellen Frequenz wird berechnet
         index = int(current_freq / df)
 
+        # Phasen werden in Grad umgerechnet
         phase_voltage_dut = (360 / (2*math.pi)) * phases_voltage_dut[index]
         phase_current = (360 / (2*math.pi)) * phases_current[index]
 
@@ -114,15 +131,12 @@ for folder in folder_paths:
         print(f"Magnitude of Impedance: {magnitude_impedance_dut} Ohm")
         print(f"Phase of Impedance: {phase_impedance_dut} degrees")
 
-        # plt.plot(frequency_vector, magnitudes_voltage_dut)
-        # plt.plot(frequency_vector, magnitudes_current)
-        # plt.xlim(0,current_freq*2)
-        # plt.show()
-
+        # Werte der aktuellen Frequenz werden zur Gesamtmessung hinzugefügt
         frequencies.append(current_freq)
         magnitudes.append(magnitude_impedance_dut)
         phases.append(phase_impedance_dut)
         
+# Visualisiert den Amplituden und Phasengang der Messung
 fig, ax = plt.subplots(2, 1, figsize=(8, 6))
 plt.subplots_adjust(hspace=0.5)
 
