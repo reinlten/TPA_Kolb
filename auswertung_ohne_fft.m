9b1258750e8662ce0b42163839e09e625760b9c6
Linus <122111700+nusli7@users.noreply.github.com>
2023-05-10 17:37:26 +0200

    Update auswertung_ohne_fft.m
    +nyquist diagramm

--- auswertung_ohne_fft.m
+++ auswertung_ohne_fft.m
@@ -2,7 +2,7 @@ clc
 clear
 
 %Öffnen der Dateien
-files = dir(fullfile('C:\Users\linus\Uni\Teamprojektarbeit\Messdaten\07.02.2023_1\1kHz - 9kHz', '*.txt*'));
+files = dir(fullfile('C:\Users\linus\Uni\Teamprojektarbeit\Messdaten\07.02.2023_1kOhm\1kHz - 9kHz', '*.txt*'));
 
 
 for file = 1:size(files)
@@ -18,8 +18,8 @@ for file = 1:size(files)
 
     %Extrahieren der Messwerte in zwei Spannungsvektoren
     data = fscanf(fileId, '%d', [dataPoints*2, 1]);
-    voltage1 = data(1:2:end);
-    voltage2 = data(2:2:end);
+    voltage1 = data(1:2:end)/1000;
+    voltage2 = data(2:2:end)/1000;
 
     fclose(fileId);
 
@@ -34,26 +34,26 @@ for file = 1:size(files)
     %Berechnung des Stroms: I = U/R
     current = voltage2/100;
 
-    %Plotten der gemessenen Spannungen und des Stroms
-    figure("Name", f + " Hz");
-    tiledlayout(2, 1);
-    nexttile;
-    plot(tVector, voltage1, tVector, voltage2, tVector, voltageDut);
-    title('Spannungen in mV');
-    xlabel ('Zeit/s');
-    ylabel ('Spannung/mV');
-    legend('Uges', 'Ushunt', 'Udut')
-
-    nexttile;
-    plot(tVector, current);
-    title('Strom in mA')
+    % %Plotten der gemessenen Spannungen und des Stroms
+    % figure("Name", f + " Hz");
+    % tiledlayout(2, 1);
+    % nexttile;
+    % plot(tVector, voltage1, tVector, voltage2, tVector, voltageDut);
+    % title('Spannungen in mV');
+    % xlabel ('Zeit/s');
+    % ylabel ('Spannung/mV');
+    % legend('Uges', 'Ushunt', 'Udut')
+    % 
+    % nexttile;
+    % plot(tVector, current);
+    % title('Strom in mA')
 
     %Berechnung des Phasenspektrums der Impedanz
     [v1Max, i] = max(voltage1);
     [v2Max, j] = max(voltage2);
     phi1 = tVector(i);
     phi2 = tVector(j);
-    phase(file, 1) = phi2 - phi1;
+    phase(file, 1) = (phi2 - phi1)*2*pi/(1/f);
     phase(file, 2) = f;
 
     %Entfernen der Spannungseinbrüche und damit korrektes Berechnen der
@@ -64,10 +64,14 @@ for file = 1:size(files)
     %Berechnen des Amplitudenspektrums der Impedanz
     magImp(file, 1) = magVDut ./ magCurrent;
     magImp(file, 2) = f;
+
+    %Berechnung von Real- und Imaginärteil
+    ImpC(file, 1) = magImp(file, 1)*cos(phase(file, 1));
+    ImpC(file, 2) = magImp(file, 1)*sin(phase(file, 1));
 end
 
-    %Plotten von Betrag und Phase
-    figure;
+    %Plotten von Betragsgang und Phasengang
+    figure("Name", "Bode-Diagramm");
     tiledlayout(2,1);
     nexttile;
     plot(magImp(:, 2), magImp(:, 1), "o");
@@ -77,6 +81,17 @@ end
 
     nexttile;
     plot(phase(:, 2), phase(:, 1), "o");
+    axis();
     xlabel("Frequenz/Hz");
     ylabel("Phase");
     title("Phasengang");
+
+    
+
+    %Plotten vom Nyquist-Diagramm
+    figure("Name", "Nyquist-Diagramm");
+    plot(ImpC(:, 1), ImpC(:, 2), "o");
+    xlabel("Re");
+    ylabel("Im");
+    title("Nyquist-Diagramm")
+    
