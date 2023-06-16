dea21d741fa84ce9204051a6e74b8c4688d4106c
Jonas Kolb <jonas.kolb28@gmail.com>
2023-06-16 21:36:15 +0200

    added phase diagramm and axis titles

--- real_simulation_single.py
+++ real_simulation_single.py
@@ -4,30 +4,32 @@ import numpy as np
 from tqdm import tqdm
 
 def main():
-
-    
     # Define frequency boundaries
     min_frequency = 1
     max_frequency = 1000000
 
     input_voltage = 550
 
-    Rdut = 120
-    Rs = 120
+    Rdut = 1000
+    Rs = 1000
 
-    Ro = 1000
+    Ro = 1000000
     Co = 4*10**-12
 
     # Define arrays for the resulting impedance values and frequencies
-    results = []
     frequencies = []
+    amplitudes = []
+    phases = []
     voltages = []
 
     for freq in tqdm(range(min_frequency, max_frequency)):
         # print(freq)
-        result = (Rs * Ro) / math.sqrt(((Rs * Ro) + Rdut * (Rs + Ro))**2 + (2 * math.pi * freq * Rs * Ro * Rdut * Co)**2)
-        results.append(result)
-        voltages.append(input_voltage * result)
+        amplitude = (Rs * Ro) / math.sqrt(((Rs * Ro) + Rdut * (Rs + Ro))**2 + (2 * math.pi * freq * Rs * Ro * Rdut * Co)**2)
+        amplitudes.append(amplitude)
+        phase = (-1)*np.arctan(((2 * np.pi * freq) * Rs * Ro * Rdut * Co) / (Rs * Ro + Rdut * (Rs + Ro)))
+        phases.append(phase)
+        
+        voltages.append(input_voltage * amplitude)
         frequencies.append(freq)
     
 
@@ -48,23 +50,22 @@ def main():
     plt.subplots_adjust(hspace=0.5)
 
     # Plot the real and imaginary parts of the complex values
-    ax[0].semilogx(frequencies, voltages)
-    # ax[0].set_xlabel('Real')
-    # ax[0].set_ylabel('Imaginary')
-    # ax[0].grid(True)
+    ax[0].semilogx(frequencies, amplitudes)
+    ax[0].set_xlabel('Frequencies in Hz')
+    ax[0].set_ylabel('Amplitude of U2 / U0')
+    ax[0].grid(True)
 
     # Plot the magnitude response on a logarithmic scale
-    ax[1].semilogx(frequencies, 20*np.log10(voltages))
-    # ax[1].plot(frequencies, mag, 'b')
-    # ax[1].set_xlabel('Frequency (Hz)')
-    # ax[1].set_ylabel('Magnitude (dB)')
-    # ax[1].grid(True)
+    ax[1].semilogx(frequencies, phases)
+    ax[1].set_xlabel('Frequency in Hz')
+    ax[1].set_ylabel('Phase of U2 / U0')
+    ax[1].grid(True)
 
     # Plot the phase response on a logarithmic scale
-    ax[2].semilogx(frequencies, results)
-    # ax[2].set_xlabel('Frequency (Hz)')
-    # ax[2].set_ylabel('Phase (rad)')
-    # ax[2].grid(True)
+    ax[2].semilogx(frequencies, voltages)
+    ax[2].set_xlabel('Frequency in Hz')
+    ax[2].set_ylabel('Voltage in mV')
+    ax[2].grid(True)
 
     plt.show()
 
